80373a05a38e77e2cbeeb89f22c8fbf2cdb0a983
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-22 16:37:53 +0200

    add productlist templates

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import Product_List,Product_Detail
+from .views import Product_List,Product_Detail,Brand_detail,Brand_list
 
 
 urlpatterns = [
+    
+    path('brands',Brand_list.as_view()),
+    path('brands/<slug:slug>',Brand_detail.as_view()),
+
     path('',Product_List.as_view()),
     path('<slug:slug>',Product_Detail.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Product
+from .models import Product,Brand
 
 
 class Product_List(ListView):
     model=Product
+    template_name='product/product_list.html'
+
 
 class Product_Detail(DetailView):
     model=Product
+    template_name='product/product_detail.html'
+
+class Brand_list(ListView):
+    model=Brand
+    template_name='product/brand_list.html'
+
+class Brand_detail(DetailView):
+    model=Brand
+    template_name='product/brand_detail.html'
